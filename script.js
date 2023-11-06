d44ce73fb86102a2dd542afcae473a80dbc4e86b
IndieDev <tblyduc2412@gmail.com>
2023-11-06 21:20:57 +0700

    Planet and Game class

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+class Planet {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
+    this.radius = 80;
+    this.image = planet;
+  }
+
+  draw(context) {
+    context.drawImage(this.image, this.x - 100, this.y - 100);
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.stroke();
+  }
+}
+
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.planet = new Planet(this);
+  }
+
+  render(context) {
+    this.planet.draw(context);
+  }
+}
+
 window.addEventListener("load", function () {
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 800;
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 800;
+  ctx.strokeStyle = "white";
+
+  const game = new Game(canvas);
+  game.render(ctx);
 });
