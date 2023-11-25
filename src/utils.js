d95c8dec896912fccc46b7f9f3230a8e8fe5d33e
IndieDev <tblyduc2412@gmail.com>
2023-11-25 19:10:30 +0700

    add Favicon and draw Planet in Start Screen.

--- src/utils.js
+++ src/utils.js
@@ -15,24 +15,32 @@ export function formatMinutes(minutes) {
 
 export function drawStartScreen(ctx, canvas) {
   ctx.save();
-    ctx.drawImage(
-      background,
-      0,
-      0,
-      800,
-      800,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-    ctx.font = `${32 * canvas.scale}px Impact`;
-    ctx.textAlign = "center";
-    ctx.fillStyle = "white";
-    ctx.fillText(
-      "Press enter to play game",
-      canvas.width / 2,
-      canvas.height / 2
-    );
-    ctx.restore();
+  ctx.drawImage(background, 0, 0, 800, 800, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    planet,
+    canvas.width * 0.5 - 200 * 0.6 * 0.5 * canvas.scale,
+    canvas.height * 0.5 - 200 * 0.6 * 0.5 * canvas.scale,
+    200 * canvas.scale * 0.6,
+    200 * canvas.scale * 0.6
+  );
+
+  ctx.textAlign = "center";
+  ctx.fillStyle = "white";
+  ctx.font = `${48 * canvas.scale}px Impact`;
+  ctx.fillText("Planet Defense", canvas.width / 2, canvas.height / 3);
+
+  ctx.font = `${32 * canvas.scale}px Consolas`;
+  ctx.fillStyle = "black";
+  ctx.fillText(
+    "Press enter to play game",
+    canvas.width / 2 - 2,
+    canvas.height / 2 + 100
+  );
+  ctx.fillStyle = "white";
+  ctx.fillText(
+    "Press enter to play game",
+    canvas.width / 2,
+    canvas.height / 2 + 102
+  );
+  ctx.restore();
 }
