66f5629be7e908939deef0e811c15ade9523101d
IndieDev <tblyduc2412@gmail.com>
2023-12-01 21:34:27 +0700

    start screen mobile message

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,20 @@
+import { mobileCheck } from "./detect-mobile-browser.js";
+
 // Function to format minutes, adding a leading zero if the minutes are below 10
 export function formatMinutes(minutes) {
-  // Convert to an integer
   minutes = parseInt(minutes);
 
-  // Check if the minutes are below 10
   if (minutes < 10) {
-    // Add a leading zero and convert to a string
     return "0" + minutes.toString();
   } else {
-    // Return the minutes unchanged if 10 or greater
     return minutes.toString();
   }
 }
 
 export function drawStartScreen(ctx, canvas) {
+  const desktopMessage = "Press enter to play game";
+  const mobileMessage = "Swipe to play game";
+  const isMobile = mobileCheck();
   ctx.save();
   ctx.drawImage(background, 0, 0, 800, 800, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(
@@ -29,18 +30,18 @@ export function drawStartScreen(ctx, canvas) {
   ctx.font = `${48 * canvas.scale}px Impact`;
   ctx.fillText("Planet Defense", canvas.width / 2, canvas.height / 3);
   ctx.fillStyle = "#D2DE32";
-  ctx.fillText("Planet Defense", canvas.width / 2+2, canvas.height / 3+2);
+  ctx.fillText("Planet Defense", canvas.width / 2 + 2, canvas.height / 3 + 2);
 
   ctx.font = `${32 * canvas.scale}px Consolas`;
   ctx.fillStyle = "black";
   ctx.fillText(
-    "Press enter to play game",
+    isMobile ? mobileMessage : desktopMessage,
     canvas.width / 2 - 2,
     canvas.height / 2 + 100
   );
   ctx.fillStyle = "white";
   ctx.fillText(
-    "Press enter to play game",
+    isMobile ? mobileMessage : desktopMessage,
     canvas.width / 2,
     canvas.height / 2 + 102
   );
